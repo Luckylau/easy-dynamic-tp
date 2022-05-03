1a0ec5c1e3def000586a8df107e25e9df3de6dc3
luckylau <laujunbupt0203@163.com>
2022-05-03 10:11:33 +0800

    fix: DtpQueue constructor

--- easy-dynamic-tp-common/src/main/java/com/luckylau/easy/dynamic/tp/common/model/DtpQueue.java
+++ easy-dynamic-tp-common/src/main/java/com/luckylau/easy/dynamic/tp/common/model/DtpQueue.java
@@ -1,7 +1,6 @@
 package com.luckylau.easy.dynamic.tp.common.model;
 
 import com.luckylau.easy.dynamic.tp.common.em.QueueTypeEnum;
-import lombok.Builder;
 import lombok.Data;
 
 /**
@@ -9,9 +8,18 @@ import lombok.Data;
  * @Date 2022/5/2
  */
 @Data
-@Builder
 public class DtpQueue {
     private QueueTypeEnum queueTypeEnum;
     private int capacity;
     private boolean fair;
+
+    public DtpQueue(QueueTypeEnum queueTypeEnum, int capacity) {
+        this.queueTypeEnum = queueTypeEnum;
+        this.capacity = capacity;
+    }
+
+    public DtpQueue(boolean fair) {
+        this.queueTypeEnum = QueueTypeEnum.SYNCHRONOUS_QUEUE;
+        this.fair = fair;
+    }
 }
